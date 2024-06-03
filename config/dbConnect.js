c27fc6069173d64a74628a18a099c03a7b3da776
JessicaMMattos <jessicamartins0563@gmail.com>
2024-06-02 23:37:09 -0300

    feat: add mongo atlas and a

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,15 +1,17 @@
 import mongoose from 'mongoose';
 
 const connectToDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/mongodb-atlantida', {
+ try {
+    const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/mongodb-atlantida';
+
+    await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log('Conectado ao MongoDB!');
-  } catch (error) {
+ } catch (error) {
     console.error('Erro na conexão com o MongoDB:', error);
-  }
+ }
 };
 
 export default connectToDB;
