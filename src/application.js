a793f8a439aa3500061e36c2a2b6803114e45d98
JessicaMMattos <jessicamartins0563@gmail.com>
2024-07-31 21:22:10 -0300

    fix: adjustment in routes and maximum body size

--- src/application.js
+++ src/application.js
@@ -5,6 +5,8 @@ import routes from './routes/index.js';
 
 const app = express();
 app.use(cors())
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 connectToDB();
 
