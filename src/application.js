0f0f503d2bc73f84708102912d672775c32cff89
JessicaMMattos <jessicamartins0563@gmail.com>
2024-09-07 17:27:16 -0300

    test

--- src/application.js
+++ src/application.js
@@ -4,7 +4,11 @@ import connectToDB from '../config/dbConnect.js';
 import routes from './routes/index.js';
 
 const app = express();
-app.use(cors())
+app.use(cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: '*'
+}));
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
